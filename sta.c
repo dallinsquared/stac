2e3f8405714b7dad0aca299d42df130d2afd1bdc
Dallin Dahl <dallinsquared@gmail.com>
2016-02-08 10:29:28 -0800

    working through errors

--- sta.c
+++ sta.c
@@ -41,7 +41,7 @@
 #define RSSIZE 100
 #define STSIZE 100
 
-#define NEXT IP=disk[*rsp]++
+#define NEXT IP=disk[disk[*rsp]++]
 #define RPUSH disk[++(*rsp)]=
 #define PUSH disk[--(*tosp)]=
 #define TOS disk[*tosp]
@@ -65,6 +65,10 @@ int disk[DSIZE] = {4, DSIZE-(RSSIZE+STSIZE+1), DSIZE-1},
     *link = disk+3, w, IP, primaddr[NOT+1];
 
 char itoabuf[10] = {'\0'};
+
+int mputs(char *s){
+	return fputs(s, stdout);
+}
 int scant(char c, char *s) {  
 	for( int i = 0; ; s++, i++) {
 		*s = getchar();
@@ -110,6 +114,25 @@ int putnumstr(int *s1) {
 	return 0;
 }
 
+char *itoa(int i){
+	char *p = itoabuf;
+	if(i<0){
+		*p++ = '-';
+		i *= -1;
+	}
+	int shifter = i;
+	do{ //Move to where representation ends
+		++p;
+		shifter = shifter/10;
+	} while(shifter);
+	*p = '\0';
+	do{ //Move back, inserting digits as you go
+		*--p =(char)((i%10)+48);
+		i = i/10;
+	} while(i);
+	return itoabuf;
+}
+
 void enter(int x){
 	disk[(*dict)++] = x;
 }
@@ -123,10 +146,194 @@ void intern(int x, int imm) {
 	int ilen = slen/PACK + (slen%PACK) ? 1 : 0;
 	disk[w] = slen;
 	putnumstr(disk+w);
+	mputs(": interning ");
+	mputs(itoa(x));
 	putchar(32);
 	*dict += slen/PACK + 1;
 	enter(imm);
+	mputs("@ ");
+	puts(itoa(*dict));
 	enter(x);
+}
+
+void execute(int x) {
+	char *s;
+	mputs("executing ");
+	puts(itoa(x));
+	switch (x) {
+	case DOCOL:
+		mputs("DOCOL IP=");
+		puts(itoa(IP));
+		w = ++IP;
+		RPUSH ++IP;
+		IP = disk[w];
+		break;
+	case IMMEDIATE:
+		disk[*link+disk[*link+1]+2] = -1;
+		NEXT;
+		break;
+	case KEY:
+		PUSH getchar();
+		NEXT;
+		break;
+	case WORD:
+		puts("WORD\n");
+		w = *dict;
+		enter(0);
+		int slen = scant((char) TOS, (char *)(disk + (*dict)));
+		DROP;
+		disk[w] = slen;
+		PUSH w;
+		*dict = w;
+		NEXT;
+		break;
+	case FIND:
+		puts("FIND\n");
+		w = *link;
+		while (streql(disk+TOS, (disk+w+1))) {
+			w = disk[w];
+		}
+		if (!w) {
+			PUSH 1;
+		} else {
+			TOS = w + (dict[w+1]);
+			PUSH dict[TOS+1];
+		}
+		NEXT;
+		break;
+	case EXIT:
+		mputs("EXIT\n");
+		RDROP;
+		NEXT;
+		break;
+	case PEEK:
+		TOS = disk[TOS];
+		NEXT;
+		break;
+	case POKE:
+		disk[TOS] = NTOS;
+		DROP;
+		DROP;
+		NEXT;
+		break;
+	case LIT:
+		mputs("LIT TORS = ");
+		puts(itoa(TORS));
+		PUSH disk[TORS++];
+		mputs("TOS = ");
+		puts(itoa(TOS));
+		NEXT;
+		break;
+	case PUSNXT:
+		PUSH disk[TORS++];
+		NEXT;
+		break;
+	case BRANCH:
+		TORS = TORS+disk[++IP];
+		NEXT;
+		break;
+	case PDROP:
+		DROP;
+		NEXT;
+		break;
+	case TOR:
+		RPUSH TOS;
+		DROP;
+		NEXT;
+		break;
+	case FROMR:
+		PUSH TORS;
+		RDROP;
+		NEXT;
+		break;
+	case DUP:
+		PUSH TOS;
+		NEXT;
+		break;
+	case SWAP:
+		w = TOS;
+		TOS = NTOS;
+		NTOS = w;
+		NEXT;
+		break;
+	case ROT:
+		w = TOS;
+		TOS = disk[(*tosp)+2];
+		disk[(*tosp)+2] = NTOS;
+		NTOS = w;
+		NEXT;
+		break;
+	case PLUS:
+		TWOLEVEL(TOS + NTOS);
+		break;
+	case MINUS:
+		TWOLEVEL(NTOS - TOS);
+		break;
+	case MULT:
+		TWOLEVEL(TOS * NTOS);
+		break;
+	case DIV:
+		TWOLEVEL(NTOS / TOS);
+		break;
+	case RSHIFT:
+		NTOS >>= TOS;
+		DROP;
+		NEXT;
+		break;
+	case LSHIFT:
+		NTOS <<= TOS;
+		DROP;
+		NEXT;
+		break;
+	case LESS:
+		TWOLEVEL(NTOS < TOS ? -1 : 0);
+		break;
+	case GREAT:
+		TWOLEVEL(NTOS > TOS ? -1 : 0);
+		break;
+	case EQL:
+		TWOLEVEL(NTOS == TOS ? -1 : 0);
+		break;
+	case EMIT:
+		putchar(TOS);
+		DROP;
+		NEXT;
+		break;
+	case ATOI:
+		TOS = (int) strtol((char *)(disk+TOS),&s, 10);
+		if((int *)s == (disk+TOS)){ //this might fail, if so, we can cast the disk pointer to a char *
+			DROP;
+		}
+		NEXT;
+		break;
+	case PNUM:
+		puts(itoa(TOS));
+		DROP;
+		NEXT;
+		break;
+	case AND:
+		NTOS &= TOS;
+		DROP;
+		NEXT;
+		break;
+	case OR:
+		NTOS |= TOS;
+		DROP;
+		NEXT;
+		break;
+	case XOR:
+		NTOS ^= TOS;
+		DROP;
+		NEXT;
+		break;
+	case NOT:
+		TOS = ~TOS;
+		NEXT;
+		break;
+	default: //this should be unreachable
+		puts("execute fallthrough!\n");
+		exit(1);
+	}
 }
 
 void pinit() {
@@ -135,6 +342,7 @@ void pinit() {
 		primaddr[i] = *dict - 1;
 	}
 }
+
 void finit(){
 	//push the address of the next open cell to the stack
 	COLON(here);
@@ -262,191 +470,6 @@ void finit(){
 	IP=coldstart;
 }
 
-char* itoa(int i){
-	char *p = itoabuf;
-	if(i<0){
-		*p++ = '-';
-		i *= -1;
-	}
-	int shifter = i;
-	do{ //Move to where representation ends
-		++p;
-		shifter = shifter/10;
-	} while(shifter);
-	*p = '\0';
-	do{ //Move back, inserting digits as you go
-		*--p =(char)((i%10)+48);
-		i = i/10;
-	} while(i);
-	return itoabuf;
-}
-
-
-void execute(int x) {
-	char *s;
-	switch (x) {
-	case DOCOL:
-		w = ++IP;
-		RPUSH ++IP;
-		IP = disk[w];
-		break;
-	case IMMEDIATE:
-		disk[*link+disk[*link+1]+2] = -1;
-		NEXT;
-		break;
-	case KEY:
-		PUSH getchar();
-		NEXT;
-		break;
-	case WORD:
-		w = *dict;
-		enter(0);
-		int slen = scant((char) disk[(*tosp)++], (char *)(disk + (*dict)));
-		disk[w] = slen;
-		PUSH w;
-		*dict = w;
-		NEXT;
-		break;
-	case FIND:
-		w = *link;
-		while (streql(disk+TOS, (disk+w+1))) {
-			w = disk[w];
-		}
-		if (!w) {
-			PUSH 1;
-		} else {
-			TOS = w + (dict[w+1]);
-			PUSH dict[TOS+1];
-		}
-		NEXT;
-		break;
-	case EXIT:
-		RDROP;
-		IP = TORS++;
-		break;
-	case PEEK:
-		TOS = disk[TOS];
-		NEXT;
-		break;
-	case POKE:
-		disk[TOS] = NTOS;
-		DROP;
-		DROP;
-		NEXT;
-		break;
-	case LIT:
-		PUSH disk[NTORS++];
-		NEXT;
-		break;
-	case PUSNXT:
-		PUSH disk[NTORS++];
-		NEXT;
-		break;
-	case BRANCH:
-		TORS = TORS+disk[++IP];
-		NEXT;
-		break;
-	case PDROP:
-		DROP;
-		NEXT;
-		break;
-	case TOR:
-		RPUSH TOS;
-		DROP;
-		NEXT;
-		break;
-	case FROMR:
-		PUSH TORS;
-		RDROP;
-		NEXT;
-		break;
-	case DUP:
-		PUSH TOS;
-		NEXT;
-		break;
-	case SWAP:
-		w = TOS;
-		TOS = NTOS;
-		NTOS = w;
-		NEXT;
-		break;
-	case ROT:
-		w = TOS;
-		TOS = disk[(*tosp)+2];
-		disk[(*tosp)+2] = NTOS;
-		NTOS = w;
-		NEXT;
-		break;
-	case PLUS:
-		TWOLEVEL(TOS + NTOS);
-		break;
-	case MINUS:
-		TWOLEVEL(NTOS - TOS);
-		break;
-	case MULT:
-		TWOLEVEL(TOS * NTOS);
-		break;
-	case DIV:
-		TWOLEVEL(NTOS / TOS);
-		break;
-	case RSHIFT:
-		NTOS >>= TOS;
-		DROP;
-		NEXT;
-		break;
-	case LSHIFT:
-		NTOS <<= TOS;
-		DROP;
-		NEXT;
-		break;
-	case LESS:
-		TWOLEVEL(NTOS < TOS ? -1 : 0);
-		break;
-	case GREAT:
-		TWOLEVEL(NTOS > TOS ? -1 : 0);
-		break;
-	case EQL:
-		TWOLEVEL(NTOS == TOS ? -1 : 0);
-		break;
-	case EMIT:
-		putchar(TOS);
-		DROP;
-		NEXT;
-		break;
-	case ATOI:
-		TOS = (int) strtol((char *)(disk+TOS),&s, 10);
-		if((int *)s == (disk+TOS)){ //this might fail, if so, we can cast the disk pointer to a char *
-			DROP;
-		}
-		NEXT;
-		break;
-	case PNUM:
-		puts(itoa(TOS));
-		DROP;
-		NEXT;
-		break;
-	case AND:
-		NTOS &= TOS;
-		DROP;
-		NEXT;
-		break;
-	case OR:
-		NTOS |= TOS;
-		DROP;
-		NEXT;
-		break;
-	case XOR:
-		NTOS ^= TOS;
-		DROP;
-		NEXT;
-		break;
-	case NOT:
-		TOS = ~TOS;
-		NEXT;
-		break;
-	}
-}
-
 void sanitycheck() {
 	for (int i = 0; i < 3; i++) {
 		if (!(0 <= disk[i] && disk[i] < DSIZE)){
@@ -475,7 +498,8 @@ void sanitycheck() {
 void cycle() {
 	//sanitycheck();
 	execute(disk[IP]);
-	puts("cycle\n");
+	mputs("cycle IP = ");
+	puts(itoa(IP));
 }
 
 void main() {
